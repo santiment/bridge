3af05323f72db31bbd3d815bc4fbdb7e152de86e
LarryCao <wenloncao2@gmail.com>
2023-05-10 15:12:18 +0100

    Refactor exporter class

--- lib/wbtc/wbtc.py
+++ lib/wbtc/wbtc.py
@@ -24,28 +24,6 @@ class WBTCExporter(Exporter):
 
         return records
 
-    def insert_records(self, records):
-        """
-        Write the processed data to clickhouse
-
-        param:
-            records: Processed records generator from process function
-        """
-        logging.debug("Insert query: %s", self.insert_query)
-        if self.dry_run != 1:
-            self.write_ch_client.execute(self.insert_query, records)
-            return
-        # If dry run mode, do nothing
-        for record in records:
-            print (record)
-
-    def start_logging(self):
-        """Start logging in the exporter"""
-        logging.getLogger("clilogger")
-        logging.info("Running %s exporter", self.name)
-        logging.info("START_DT=%s", self.start_dt)
-        logging.info("END_DT=%s", self.end_dt)
-
     def run(self):
         """The main function to run the WBTC exporter"""
         self.start_logging()

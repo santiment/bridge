e978297d25c4b779a3384c6caa92bcacde85a9d5
LarryCao <wenloncao2@gmail.com>
2023-05-30 23:59:31 +0100

    Add os system and pathlib in auto_coder

--- bin/auto_coder.py
+++ bin/auto_coder.py
@@ -1,30 +1,64 @@
-class Commands:
-    pass
+import os
+import subprocess
+from subprocess import PIPE
+from pathlib import Path
+
+# class Commands:
+#     read_file
+#     wirte_file
+    
 
 class ProgramResult:
     pass
 
+def go_parent_dir():
+    curr_path = os.getcwd()
+    dirs = os.listdir(curr_path)
+    if 'bin' not in dirs:
+        os.system("cd ..")
 
-def generate_code():
-    pass
+def generate_code(bash_script):
+    os.system(bash_script)
 
-def format_code():
-    pass
+def format_code(gpt_file, project_name):
+    
+    # Locate files and dest folders
+    go_parent_dir()
+    gpt_file_path = Path('./bin') / gpt_file
+    dest_path = Path('./lib') / project_name
+    if not dest_path.exists():
+        dest_path.mkdir()
+    print ("Creating dest folders!")
+
+    # Split the gpt result file into separate files
+    with open(gpt_file_path, 'r') as f:
+        data = f.readlines()
+        print (data)
+    
 
 def fix_code():
     pass
 
-def run_program() -> ProgramResult:
-    pass
+def run_program(project_name=None):
+    go_parent_dir()
+    output = subprocess.run(["bash", "bin/run.sh", project_name], stdout=PIPE, stderr=PIPE)
+
+    print (output)
+    return output
 
 if __name__ == "__main__":
-    COMMANDS = []
-    generate_code(COMMANDS)
-    format_code()
+    bash_script = './gpt_write_code'
+    gpt_file = 'new_exporter.py'
+    project_name = 'polygon_bridge'
+    
+    result = run_program(project_name)
 
+    #generate_code(bash_script)
+    
+    format_code(gpt_file, project_name)
 
-    result = run_program()
+    result = run_program(project_name)
 
     while (result.failed()):
         fix_code(result)
-        result = run_program()
+        result = run_program(project_name)
--- lib/polygon_bridge/query.py
+++ lib/polygon_bridge/query.py
@@ -9,31 +9,37 @@ from lib.polygon_bridge.constants import (
 )
 
 
+"""
+Provide functions for clickhouse query in polygon exporter
+"""
+
+
 def build_events_query(start_dt, end_dt):
     """
     Read events from eth_events_v2 table using bridge protoccol contracts
     """
 
     query_string = f"""
-    SELECT 
-        tx_hash,
-        contract_addr,
-        args,
-        dt,
-        log_index,
-        CASE 
-            WHEN signature = '{MINT_CONFIRMED_SIG}' THEN 'mint'
-            WHEN signature = '{BURNED_CONFIRMED_SIG}' THEN 'burn'
-        END as action
+    SELECT
+    tx_hash,
+    contract_addr,
+    args,
+    dt,
+    log_index,
+    CASE
+    WHEN signature = '{MINT_CONFIRMED_SIG}' THEN 'mint'
+    WHEN signature = '{BURNED_CONFIRMED_SIG}' THEN 'burn'
+    END as action
     FROM
-        {ETH_TRANSFERS_TABLE}
+    {ETH_EVENTS_TABLE}
     WHERE
         dt >= toDateTime('{start_dt}')
         AND dt < toDateTime('{end_dt}')
-        AND contract_addr = '{WBTC_FACTORY}'
+        AND contract_addr = '{POLYGON_FACTORY}'
         AND signature IN ['{MINT_CONFIRMED_SIG}', '{BURNED_CONFIRMED_SIG}']
     ORDER BY
-        dt DESC,
-        log_index DESC
+    dt DESC,
+    log_index DESC
     """
     return query_string
+

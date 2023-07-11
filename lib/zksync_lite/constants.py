16d9193698b6b944f1abcadc0894a4defb3acc16
LarryCao <wenloncao2@gmail.com>
2023-07-11 15:27:55 +0100

    aider: The code has been updated to implement a retry mechanism for the API request using the `retry` library in Python.
    # Context:
    USER: How to make it retry if the api request is failed? Use the best practice
    ASSISTANT: To implement a retry mechanism for the API request, we can use the `retry` library in Python. This library provides a decorator `@retry` which can be used to automatically retry a function or method until it returns a value that satisfies us, or until a certain number of attempts have been made.
    
    First, you need to install the `retry` library. You can do this by running `pip install retry`.
    
    Then, we can modify the `lib/zksync_lite/constants.py` file as follows:
    
    ```python
    lib/zksync_lite/constants.py
    <<<<<<< ORIGINAL
    import requests
    import logging
    
    TOKEN_API = "https://api.zksync.io/api/v0.1/tokens"
    try:
        res = requests.get(TOKEN_API).json()
        token_dict = {x['id']:x['address'] for x in res}
        token_dict[0] = 'ETH'
    except requests.exceptions.RequestException as request_exception:
        logging.info("Error occurred while making request to TOKEN_API, %s", request_exception)
    except Exception as expt:
        logging.info("An unexpected error occurred, %s", expt)
    =======
    import requests
    import logging
    from retry import retry
    
    TOKEN_API = "https://api.zksync.io/api/v0.1/tokens"
    
    @retry(requests.exceptions.RequestException, tries=3, delay=2, backoff=2)
    def get_token_dict():
        res = requests.get(TOKEN_API).json()
        token_dict = {x['id']:x['address'] for x in res}
        token_dict[0] = 'ETH'
        return token_dict
    
    try:
        token_dict = get_token_dict()
    except requests.exceptions.RequestException as request_exception:
        logging.info("Error occurred while making request to TOKEN_API, %s", request_exception)
    except Exception as expt:
        logging.info("An unexpected error occurred, %s", expt)
    >>>>>>> UPDATED
    ```
    
    In the updated code, we have defined a new function `get_token_dict` and decorated it with `@retry`. This will automatically retry the function if it raises `requests.exceptions.RequestException`. The `tries` parameter is the maximum number of attempts, `delay` is the initial delay between retries in seconds, and `backoff` is the factor by which the delay should lengthen after each failure.

--- lib/zksync_lite/constants.py
+++ lib/zksync_lite/constants.py
@@ -10,12 +10,21 @@ DEPOSIT_SIG = '0x8f5f51448394699ad6a3b80cdadf4ec68c5d724c8c3fea09bea55b3c2d0e2dd
 WITHDRAWAL_SIG = '0xefef619ae4a542a2b8810b4efeccd8478bd683e985354ee31dd2d644aff6d0ca'
 
 # Get tokens from zksync api
-token_dict = {}
+import requests
+import logging
+from retry import retry
+
 TOKEN_API = "https://api.zksync.io/api/v0.1/tokens"
-try:
+
+@retry(requests.exceptions.RequestException, tries=3, delay=2, backoff=2)
+def get_token_dict():
     res = requests.get(TOKEN_API).json()
     token_dict = {x['id']:x['address'] for x in res}
     token_dict[0] = 'ETH'
+    return token_dict
+
+try:
+    token_dict = get_token_dict()
 except requests.exceptions.RequestException as request_exception:
     logging.info("Error occurred while making request to TOKEN_API, %s", request_exception)
 except Exception as expt:

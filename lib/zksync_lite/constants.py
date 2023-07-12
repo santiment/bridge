d94067793ccaa71b1ee378c7fa22a39048317d29
LarryCao <wenloncao2@gmail.com>
2023-07-12 14:25:31 +0100

    Refactor code to import the `requests` module and add a docstring to the `get_token_dict` function.

--- lib/zksync_lite/constants.py
+++ lib/zksync_lite/constants.py
@@ -2,22 +2,20 @@
 Provide constants to be used in zksync_era_bridge exporter
 """
 
-import requests
 import logging
+import requests
+from retry import retry
 
 ZKSYNC_LITE = "0xaBEA9132b05A70803a4E85094fD0e1800777fBEF".lower()
 DEPOSIT_SIG = '0x8f5f51448394699ad6a3b80cdadf4ec68c5d724c8c3fea09bea55b3c2d0e2dd0'
 WITHDRAWAL_SIG = '0xefef619ae4a542a2b8810b4efeccd8478bd683e985354ee31dd2d644aff6d0ca'
 
-# Get tokens from zksync api
-import requests
-import logging
-from retry import retry
 
 TOKEN_API = "https://api.zksync.io/api/v0.1/tokens"
 
 @retry(requests.exceptions.RequestException, tries=3, delay=2, backoff=2)
 def get_token_dict():
+    """Get token id info from zksync api"""
     res = requests.get(TOKEN_API).json()
     token_dict = {x['id']:x['address'] for x in res}
     token_dict[0] = 'ETH'

8a9c8551f120258e32f4ff284ac0915083fecc10
Katsiaryna <alresharin@gmail.com>
2024-02-27 17:06:46 +0300

    Remove the merge on the table avax_reciepts

--- lib/avalanche/query.py
+++ lib/avalanche/query.py
@@ -23,35 +23,12 @@ def build_events_query(start_dt, end_dt):
             value,
             contract,
             'deposit' as action
-        FROM (
-            SELECT DISTINCT
-                dt,
-                transactionHash,
-                from,
-                to,
-                value,
-                contract
-            FROM {ERC20_TRANSFERS_TABLE}
-            WHERE to = '{ETH_AVAX_BRIDGE}'
-            AND dt > toDateTime('{start_dt}')
-            AND dt < toDateTime('{end_dt}')
-
-        ) INNER JOIN (
-            SELECT avax_tx_hash, concat('0x', substr(args, 259, 64)) as transactionHash FROM (
-
-                SELECT DISTINCT transactionHash as avax_tx_hash, topics, args
-                FROM {AVAX_RECEIPTS_TABLE}
-
-                ARRAY JOIN
-                    logs.topics as topics,
-                    logs.data as args
-
-                WHERE dt > toDateTime('{start_dt}')
-                AND dt < toDateTime('{end_dt}')
-            )
-            WHERE topics[1] = '{ETH_AVAX_TRANSFER_SIG}'
-        ) USING transactionHash
-        ORDER BY dt)
+        FROM {ERC20_TRANSFERS_TABLE}
+        WHERE to = '{ETH_AVAX_BRIDGE}'
+        AND dt >= toDateTime('{start_dt}')
+        AND dt < toDateTime('{end_dt}')
+        ORDER BY dt
+        )
 
         UNION ALL
 
@@ -65,9 +42,9 @@ def build_events_query(start_dt, end_dt):
             contract,
             'withdraw' as action
         FROM {ERC20_TRANSFERS_TABLE}
-            WHERE from = '{ETH_AVAX_BRIDGE}'
-            AND dt > toDateTime('{start_dt}')
-            AND dt < toDateTime('{end_dt}')
+        WHERE from = '{ETH_AVAX_BRIDGE}'
+        AND dt >= toDateTime('{start_dt}')
+        AND dt < toDateTime('{end_dt}')
         ORDER BY dt
         )
     '''
